59c3152b56023c71f9c7e55dedf0bb0576d91106
STHELLER <31294872+STHELLER@users.noreply.github.com>
2024-06-21 16:49:53 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -44,34 +44,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// Define a variable to store the deferred prompt event
-let deferredPrompt;
-
-// Event listener for beforeinstallprompt event
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later
-  deferredPrompt = event;
-  // Show the install button or trigger the installation prompt
-  showInstallPrompt();
-});
-
-// Function to show the install button and handle user interaction
-function showInstallPrompt() {
-  const installButton = document.getElementById('installButton');
-  installButton.style.display = 'block'; // Display the install button
-  installButton.addEventListener('click', () => {
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
-}
